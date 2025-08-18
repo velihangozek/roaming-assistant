d16287a610465dc2c57e388e63d181ee74c354ba
Velihan Gözek <velihangozek@hotmail.com>
2025-08-19 00:00:33 +0300

    create initial skeleton

---
+++ src/main/java/com/turkcell/roaming/roaming_assistant/RoamingAssistantApplication.java
@@ -0,0 +1,13 @@
+package com.turkcell.roaming.roaming_assistant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoamingAssistantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoamingAssistantApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/turkcell/roaming/roaming_assistant/RoamingAssistantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.turkcell.roaming.roaming_assistant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoamingAssistantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
